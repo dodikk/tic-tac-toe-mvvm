e7ff4c9d4d1f1a96e5c7a7ab207171102dd90e73
dodikk <dodikk88.reg@gmail.com>
2015-11-20 17:27:39 +0200

    [UI] Added game VC stub

---
+++ app/TicTacToe/TicTacToe/TTTGameVC.h
@@ -0,0 +1,13 @@
+//
+//  TTTGameVC.h
+//  TicTacToe
+//
+//  Created by Oleksandr Dodatko on 20/11/2015.
+//  Copyright © 2015 dodikk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTGameVC : UIViewController
+
+@end
