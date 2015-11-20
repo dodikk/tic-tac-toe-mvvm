6c2f84a7ab32040d80c903b0319ce6adc9a21f49
dodikk <dodikk88.reg@gmail.com>
2015-11-20 17:17:52 +0200

    Added a busyness logic project

---
+++ lib/TicTacToeKit/TicTacToeKit/TicTacToeKit.h
@@ -0,0 +1,13 @@
+//
+//  TicTacToeKit.h
+//  TicTacToeKit
+//
+//  Created by Oleksandr Dodatko on 20/11/2015.
+//  Copyright © 2015 dodikk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TicTacToeKit : NSObject
+
+@end
---
+++ lib/TicTacToeKit/TicTacToeKit/TicTacToeKit.m
@@ -0,0 +1,13 @@
+//
+//  TicTacToeKit.m
+//  TicTacToeKit
+//
+//  Created by Oleksandr Dodatko on 20/11/2015.
+//  Copyright © 2015 dodikk. All rights reserved.
+//
+
+#import "TicTacToeKit.h"
+
+@implementation TicTacToeKit
+
+@end
