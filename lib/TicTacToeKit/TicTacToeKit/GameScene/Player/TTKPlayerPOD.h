4e729f22c1f90bacde6546dd9611bbcf3ec21aa9
dodikk <dodikk88.reg@gmail.com>
2015-11-21 15:44:57 +0200

    [kit] Added a player structure

---
+++ lib/TicTacToeKit/TicTacToeKit/GameScene/Player/TTKPlayerPOD.h
@@ -0,0 +1,20 @@
+//
+//  TTKPlayerPOD.h
+//  TicTacToeKit
+//
+//  Created by Oleksandr Dodatko on 21/11/2015.
+//  Copyright © 2015 dodikk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TTKPlayer.h"
+
+@class UIImage;
+
+@interface TTKPlayerPOD : NSObject<TTKPlayer>
+
+@property (nonatomic) BOOL isPlayerX;
+@property (nonatomic) BOOL isPlayerO;
+@property (nonatomic) UIImage* imageForPosessedCell;
+
+@end
---
+++ lib/TicTacToeKit/TicTacToeKit/GameScene/Player/TTKPlayerPOD.m
@@ -0,0 +1,13 @@
+//
+//  TTKPlayerPOD.m
+//  TicTacToeKit
+//
+//  Created by Oleksandr Dodatko on 21/11/2015.
+//  Copyright © 2015 dodikk. All rights reserved.
+//
+
+#import "TTKPlayerPOD.h"
+
+@implementation TTKPlayerPOD
+
+@end
