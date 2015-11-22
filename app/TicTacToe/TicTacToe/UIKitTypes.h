f870e459ed91bbc2fad85f7e31583d01c28d9176
dodikk <dodikk88.reg@gmail.com>
2015-11-22 02:05:06 +0200

    Added UIKit typedefs header

---
+++ app/TicTacToe/TicTacToe/UIKitTypes.h
@@ -0,0 +1,16 @@
+//
+//  UIKitTypes.h
+//  TicTacToe
+//
+//  Created by Oleksandr Dodatko on 22/11/2015.
+//  Copyright © 2015 dodikk. All rights reserved.
+//
+
+#ifndef UIKitTypes_h
+#define UIKitTypes_h
+
+#import <UIKit/UIKit.h>
+
+typedef void (^UIAlertControllerButtonHandler)(UIAlertAction *action);
+
+#endif /* UIKitTypes_h */
